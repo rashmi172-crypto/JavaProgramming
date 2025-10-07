3708069011e8340a2b0a1c83724a178084b71c50
rashmi172-crypto <rashmipatel172@gmail.com>
2025-10-07 17:13:42 +0530

    Add files via upload

---
+++ Polymorphism/HelperDriver.java
@@ -0,0 +1,10 @@
+package com.Polymorphism;
+
+public class HelperDriver {
+	public static void main(String[] args) {
+		
+		System.out.println(Helper.Multiply(5, 10));
+		System.out.println(Helper.Multiply(5.5, 6.3));
+	}
+
+}
