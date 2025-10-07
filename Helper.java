47a2cdade2a5cf3a735e3d2eb4369923cf5223cf
rashmi172-crypto <rashmipatel172@gmail.com>
2025-10-07 17:03:37 +0530

    Add files via upload

---
+++ Helper.java
@@ -0,0 +1,13 @@
+package com.Polymorphism;
+
+public class Helper {
+	
+	static int Multiply(int a, int b) 
+	{
+		return a*b;
+	}
+    static double Multiply(double a,double b)
+    {
+    	return a*b;
+    }
+}
