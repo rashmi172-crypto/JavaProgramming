16fa72338c7f69756951e5e4c8b176cca04be04f
rashmi172-crypto <rashmipatel172@gmail.com>
2025-10-27 15:55:15 +0530

    Update NumberGame.java

--- NumberGuessingGame/NumberGame.java
+++ NumberGuessingGame/NumberGame.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 
 public class NumberGame {
 	public static void gussingNumberGame() {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc=null;
+		try{
+		sc = new Scanner(System.in);
 		int number = 1+(int)(100*Math.random());
 		int k = 5;
 		System.out.println("A number choose between 1 to 100");
 		System.out.println("you have" + k + "attemps to correct the guess number" );
-		try{
+		
 		for(int i=0;i<k;i++) {
 			System.out.println("enter your guess number: ");
 			int guess = sc.nextInt();
@@ -24,17 +26,18 @@ public class NumberGame {
 				System.out.println("the number is smaller than " + guess);
 			}
 		}
-		}catch(Exception e){
-			e.printStackTrace();}
-		finally {
-			System.out.println("Finally block");
-		}
+		
 		
 		System.out.println("you have exhausted all attemps. the correct number was " + number);
+		} catch(Exception e){
+			e.printStackTrace();
+		} finally{
 		sc.close();
+	}
 	}
 public static void main(String[] args) {
 	gussingNumberGame();
 }
 }
+
 
